6e591594226f8093a329e7d896b50313ff1fef79
Chieudepzaj <chieumu1999@gmail.com>
2021-05-20 17:00:42 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyAbR45lrzaZ33BtikqNBjtvnrHeWQaxiuk",
+    authDomain: "fir-sample-6629a.firebaseapp.com",
+    projectId: "fir-sample-6629a",
+    storageBucket: "fir-sample-6629a.appspot.com",
+    messagingSenderId: "639402109892",
+    appId: "1:639402109892:web:6209c24a6c6eaaa3f01291"
+  };
+
+firebase.initializeApp(firebaseConfig);
